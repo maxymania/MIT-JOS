b5b447f101f80193ce1f3113dee8bb6b578979a9
Ben Wei <ben@staros.mobi>
2012-06-10 14:47:19 +0800

    ex10: set_pgfault_handler in lib/pgfault.c

--- kern/pmap.c
+++ kern/pmap.c
@@ -635,6 +635,10 @@ user_mem_check(struct Env *env, const void *va, size_t len, int perm)
 	do {
 		addr += i*PGSIZE;
 		pte = pgdir_walk(env->env_pgdir, (void*) addr, 0); 
+		if (!pte) {
+			user_mem_check_addr = i ? addr : (uintptr_t) va;
+			return -E_FAULT;
+		}
 		if ((PGSIZE + addr) > ULIM
 		     || ((*pte & perm) != perm) ) {
 			user_mem_check_addr = i ? addr : (uintptr_t) va;
